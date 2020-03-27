7c27f66658b7ba5c40374de3c22b149a3272cedb
Philipp Spilger <philipp.spilger@kip.uni-heidelberg.de>
2020-03-27 18:15:26 +0100

    Enhance serialization test to test all archive types
    Depends-On: 10189
    Change-Id: I971953bfd5f49e6b3b32e97516ef6201f44cb2d9

--- tests/sw/hxcomm/test-serialization_cereal.cpp
+++ tests/sw/hxcomm/test-serialization_cereal.cpp
@@ -2,7 +2,10 @@
 
 #include "hxcomm/common/cerealization_utmessage.h"
 
+#include <cereal/archives/binary.hpp>
 #include <cereal/archives/json.hpp>
+#include <cereal/archives/portable_binary.hpp>
+#include <cereal/archives/xml.hpp>
 
 #include "test-to_testing_types.h"
 
@@ -30,23 +33,32 @@ TYPED_TEST(CommonSerializationTests, IsAssignable)
 	obj1 = obj2;
 }
 
-TYPED_TEST(CommonSerializationTests, HasSerialization)
-{
-	TypeParam obj1, obj2;
-
-	std::ostringstream ostream;
-	{
-		cereal::JSONOutputArchive oa(ostream);
-		oa(obj1);
+#define SERIALIZATION_TEST(OArchive, IArchive)                                                     \
+	TYPED_TEST(CommonSerializationTests, HasSerialization##IArchive##OArchive)                     \
+	{                                                                                              \
+		TypeParam obj1, obj2;                                                                      \
+                                                                                                   \
+		using namespace cereal;                                                                    \
+		std::ostringstream ostream;                                                                \
+		{                                                                                          \
+			OArchive oa(ostream);                                                                  \
+			oa(obj1);                                                                              \
+		}                                                                                          \
+                                                                                                   \
+		std::istringstream istream(ostream.str());                                                 \
+		{                                                                                          \
+			IArchive ia(istream);                                                                  \
+			ia(obj2);                                                                              \
+		}                                                                                          \
+		/* This does only test that Serialization does not insert wrong values                     \
+		   but does not check coverage since both instances are default constructed.               \
+		   Coverage check is done in each container's test file. */                                \
+		ASSERT_EQ(obj2, obj1);                                                                     \
 	}
 
-	std::istringstream istream(ostream.str());
-	{
-		cereal::JSONInputArchive ia(istream);
-		ia(obj2);
-	}
-	// This does only test that Serialization does not insert wrong values
-	// but does not check coverage since both instances are default constructed.
-	// Coverage check is done in each container's test file.
-	ASSERT_EQ(obj2, obj1);
-}
+SERIALIZATION_TEST(BinaryOutputArchive, BinaryInputArchive)
+SERIALIZATION_TEST(PortableBinaryOutputArchive, PortableBinaryInputArchive)
+SERIALIZATION_TEST(JSONOutputArchive, JSONInputArchive)
+SERIALIZATION_TEST(XMLOutputArchive, XMLInputArchive)
+
+#undef SERIALIZATION_TEST
