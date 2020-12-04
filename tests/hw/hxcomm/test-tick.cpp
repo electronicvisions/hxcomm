0bcf9d1ac4dc17f752721765436c35f16e39f954
Eric Müller <mueller@kip.uni-heidelberg.de>
2020-12-04 21:30:54 +0100

    Add TestConnection.TickPerformance
    Change-Id: I587e90590422419ded8e484991d2d75722e18aec

--- tests/hw/hxcomm/test-tick.cpp
+++ tests/hw/hxcomm/test-tick.cpp
@@ -65,3 +65,39 @@ TEST(TestConnection, TickRestrictedStream)
 	auto connection = get_connection_from_env();
 	std::visit(test, connection);
 }
+
+TEST(TestConnection, TickPerformance)
+{
+	using namespace hxcomm::vx;
+	using namespace hxcomm::vx::instruction;
+
+	auto const test = [](auto& connection, size_t const N) {
+		// N ticks
+		std::vector<hxcomm::vx::UTMessageToFPGAVariant> messages(
+		    N, UTMessageToFPGA<system::Loopback>(system::Loopback::tick));
+
+		// halt message added by execute_messages
+		auto [responses, connection_time_info] = hxcomm::execute_messages(connection, messages);
+
+		std::cout << connection_time_info << std::endl;
+
+		EXPECT_EQ(responses.size(), N + 1);
+		auto response_tick = responses.at(0);
+		EXPECT_EQ(
+		    std::get<UTMessageFromFPGA<from_fpga_system::Loopback>>(response_tick),
+		    UTMessageFromFPGA<from_fpga_system::Loopback>(from_fpga_system::Loopback::tick));
+		auto response_halt = responses.at(N);
+		EXPECT_EQ(
+		    std::get<UTMessageFromFPGA<from_fpga_system::Loopback>>(response_halt),
+		    UTMessageFromFPGA<from_fpga_system::Loopback>(from_fpga_system::Loopback::halt));
+	};
+
+	auto connection = get_connection_from_env();
+
+	// TODO: cf. issue #3811
+	std::vector<size_t> sizes({1, 1, 1, 10, 100, 1000, 10'000, 100'000, 1'000'000});
+	for (size_t const n : sizes) {
+		auto test_n = [&test, n](auto& connection) { test(connection, n); };
+		std::visit(test_n, connection);
+	}
+}
