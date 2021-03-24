6b38f8883365f65bdec69ed10bb64ac3d235c94c
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2021-03-24 00:05:01 +0000

    Add debug messages for SF<->cereal serialization
    Change-Id: I958945d57f9bac9529a5b9f4132fab8d885ef9dc

--- include/hxcomm/common/sf_serialization.h
+++ include/hxcomm/common/sf_serialization.h
@@ -9,6 +9,10 @@
 
 #include "cereal/archives/portable_binary.hpp"
 
+// time statistics
+#include "hate/timer.h"
+#include "hxcomm/common/logger.h"
+
 namespace SF {
 
 /**
@@ -17,29 +21,43 @@ namespace SF {
 template <class T>
 void translate_sf_cereal(Archive& ar, T& cerealizable)
 {
+	static auto log = log4cxx::Logger::getLogger(__func__);
+
 	if (ar.isWrite()) {
 		std::ostringstream os;
 		{
 			cereal::PortableBinaryOutputArchive cereal_archive(os);
 
+			[[maybe_unused]] hate::Timer timer;
 			cereal_archive(cerealizable);
+			HXCOMM_LOG_DEBUG(log, "cereal (object -> archive): " << timer.print());
 		}
 
-		// clang-format off
-		ar & os.str();
-		// clang-format on
+		{
+			[[maybe_unused]] hate::Timer timer;
+			// clang-format off
+			ar & os.str();
+			// clang-format on
+			HXCOMM_LOG_DEBUG(log, "SF (object -> archive): " << timer.print());
+		}
 
 	} else if (ar.isRead()) {
 		std::string buf;
-		// clang-format off
-		ar & buf;
-		// clang-format on
+		{
+			[[maybe_unused]] hate::Timer timer;
+			// clang-format off
+			ar & buf;
+			// clang-format on
+			HXCOMM_LOG_DEBUG(log, "SF (archive -> object): " << timer.print());
+		}
 
 		std::istringstream is(buf);
 		{
 			cereal::PortableBinaryInputArchive cereal_archive(is);
 
+			[[maybe_unused]] hate::Timer timer;
 			cereal_archive(cerealizable);
+			HXCOMM_LOG_DEBUG(log, "cereal (archive -> object): " << timer.print());
 		}
 
 	} else {
