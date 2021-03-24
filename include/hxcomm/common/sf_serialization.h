2b7dabe9b54a4760fb6b3d155b6901bf2de3cf31
Oliver Breitwieser <oliver.breitwieser@kip.uni-heidelberg.de>
2021-03-24 00:05:01 +0000

    Add debug messages for SF<->cereal serialization
    Change-Id: I958945d57f9bac9529a5b9f4132fab8d885ef9dc

--- include/hxcomm/common/sf_serialization.h
+++ include/hxcomm/common/sf_serialization.h
@@ -9,6 +9,10 @@
 
 #include "cereal/archives/portable_binary.hpp"
 
+// time statistics
+#include "hxcomm/common/logger.h"
+#include <chrono>
+
 namespace SF {
 
 /**
@@ -17,29 +21,63 @@ namespace SF {
 template <class T>
 void translate_sf_cereal(Archive& ar, T& cerealizable)
 {
+	auto log = log4cxx::Logger::getLogger(__func__);
+
 	if (ar.isWrite()) {
 		std::ostringstream os;
 		{
 			cereal::PortableBinaryOutputArchive cereal_archive(os);
 
+			[[maybe_unused]] auto start = std::chrono::high_resolution_clock::now();
 			cereal_archive(cerealizable);
+			[[maybe_unused]] auto stop = std::chrono::high_resolution_clock::now();
+			HXCOMM_LOG_DEBUG(
+			    log,
+			    "cereal (object -> archive): "
+			        << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()
+			        << "ms");
 		}
 
-		// clang-format off
-		ar & os.str();
-		// clang-format on
+		{
+			[[maybe_unused]] auto start = std::chrono::high_resolution_clock::now();
+			// clang-format off
+			ar & os.str();
+			// clang-format on
+			[[maybe_unused]] auto stop = std::chrono::high_resolution_clock::now();
+			HXCOMM_LOG_DEBUG(
+			    log,
+			    "SF (object -> archive): "
+			        << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()
+			        << "ms");
+		}
 
 	} else if (ar.isRead()) {
 		std::string buf;
-		// clang-format off
-		ar & buf;
-		// clang-format on
+		{
+			[[maybe_unused]] auto start = std::chrono::high_resolution_clock::now();
+			// clang-format off
+			ar & buf;
+			// clang-format on
+			[[maybe_unused]] auto stop = std::chrono::high_resolution_clock::now();
+			HXCOMM_LOG_DEBUG(
+			    log,
+			    "SF (archive -> object): "
+			        << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()
+			        << "ms");
+		}
 
 		std::istringstream is(buf);
 		{
 			cereal::PortableBinaryInputArchive cereal_archive(is);
 
+			[[maybe_unused]] auto start = std::chrono::high_resolution_clock::now();
 			cereal_archive(cerealizable);
+			[[maybe_unused]] auto stop = std::chrono::high_resolution_clock::now();
+			HXCOMM_LOG_DEBUG(
+			    log,
+			    "cereal (archive -> object): "
+			        << std::chrono::duration_cast<std::chrono::milliseconds>(stop - start).count()
+			        << "ms");
 		}
 
 	} else {
