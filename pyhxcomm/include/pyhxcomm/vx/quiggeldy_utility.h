ded34f2ac03103e48e45ed730f9d923eb54aa23b
Christian Pehle <christian.pehle@kip.uni-heidelberg.de>
2022-03-15 16:01:38 +0100

    expose launching quiggeldy locally to python
    Change-Id: Ia13a4d64cd21615ba7780dd71825d84097e6f5a0

--- pyhxcomm/include/pyhxcomm/vx/quiggeldy_utility.h
+++ pyhxcomm/include/pyhxcomm/vx/quiggeldy_utility.h
@@ -9,6 +9,8 @@ namespace pyhxcomm::vx GENPYBIND_TAG_HXCOMM_VX {
 GENPYBIND_MANUAL({
 	parent.def("get_unused_port", &::hxcomm::get_unused_port);
 	parent.def("terminate", &::hxcomm::terminate);
+	parent.def(
+	    "launch_quiggeldy_locally_from_env", &::hxcomm::vx::launch_quiggeldy_locally_from_env);
 })
 
 } // namespace pyhxcomm::vx
