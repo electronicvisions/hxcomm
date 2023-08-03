07064042ad46799a8acfa6e89f6af14f7fced2d0
Yannik Stradmann <yannik.stradmann@kip.uni-heidelberg.de>
2023-08-03 11:22:28 +0200

    Allow chip version 4 in JTAG tests
    Change-Id: I43f05a04afe99ef14efa3ae26d91a7f0ad061d75

--- tests/hw/hxcomm/test-readout_jtag_id.cpp
+++ tests/hw/hxcomm/test-readout_jtag_id.cpp
@@ -49,5 +49,7 @@ TEST(TestConnection, ReadoutJtagID)
 	EXPECT_EQ(responses.size(), 2);
 	auto jtag_id = static_cast<uint32_t>(
 	    std::get<UTMessageFromFPGA<jtag_from_hicann::Data>>(responses.front()).decode());
-	EXPECT_TRUE((jtag_id == 0x048580AF) || (jtag_id == 0x248580AF) || (jtag_id == 0x348580AF));
+	EXPECT_TRUE(
+	    (jtag_id == 0x048580AF) || (jtag_id == 0x248580AF) || (jtag_id == 0x348580AF) ||
+	    (jtag_id == 0x448580AF));
 }
